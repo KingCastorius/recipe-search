7d64b89c054593a7fe3a13bda81a3c308a53c044
KingCastorius <malizakzorz@gmail.com>
2017-10-24 16:21:21 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+let key = 0;
+
+class Request extends React.Component {
+	state = {
+		rName: '',
+		returnedRecipes: []
+	}
+
+	getRecipe(e) {
+		e.preventDefault()
+		axios.get('http://food2fork.com/api/get?q=' + this.state.rName).then((res) => {
+			let recipe = res.data.items.map(item => <li key={key++}>{item.git_url}</li>)
+			this.setState({returnedRecipes: recipe})
+		})
+	}
+	
+
+	render() {
+		return (
+			<span>
+				<form onSubmit={(e) => this.getRecipe(e)}>
+					<input 
+						name="rName"
+						type="text" 
+						placeholder="ingredients I have" 
+						value={this.state.rName} 
+						onChange={(e) => this.setState({[e.target.name]: e.target.value})} />
+					<button type="submit">Submit</button>
+				</form>
+
+				<ul>
+					{this.state.returnedRecipes}
+				</ul>
+			</span>
+		)
+	}
+}
+
+ReactDOM.render(
+	<Request />,
+	document.getElementById('root')
+)
